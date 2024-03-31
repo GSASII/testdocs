f71f56c905587c6f29b85bd40cea681c04edf997
BHT <toby@anl.gov>
2024-03-31 08:29:12 -0500

    setup as mini-version of GSAS-II-tutorials

--- webdocs/conf.py
+++ webdocs/conf.py
@@ -6,16 +6,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'GSAS-II web documentation'
+copyright = '2024 by Argonne National Laboratory'
+author = 'Brian H. Toby'
 release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+# extensions = ['myst_parser']
+extensions = ['sphinxnotes.strike']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -25,4 +26,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
